8071c4626eaaa105fc0c9849f101eb52f28e36b2
lkovac <lukakovac153@gmail.com>
2017-08-26 12:47:46 +0200

    Add project files.

---
+++ XMLandWebServices/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using XMLandWebServices.ServiceReference1;
+
+namespace XMLandWebServices
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int counter = 0;
+        private Service1Client service;
+        public MainWindow()
+        {
+            InitializeComponent();
+            service = new Service1Client();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var stringResult = service.GetData(counter);
+            counter++;
+            ButtonView.Content = stringResult;
+        }
+    }
+}
