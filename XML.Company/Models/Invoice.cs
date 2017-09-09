662eb060a4810355af1171be0991e3f5faedaeb0
lkovac <lukakovac153@gmail.com>
2017-09-09 18:33:28 +0200

    New architecture commit

---
+++ XML.Company/Models/Invoice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XML.Company.Models
+{
+    public class Invoice
+    {
+        public string MessageId { get; set; }
+
+        public string SupplierName { get; set; }
+
+        public string SupplierAddress { get; set; }
+
+        public string PIBSupplier { get; set; }
+
+        public string BuyerName { get; set; }
+
+        public string BuyerAddress { get; set; }
+
+        public string BuyerAccount { get; set; }
+
+        public string BillingDate { get; set; }
+
+        public string GoodsValue { get; set; }
+    }
+}
